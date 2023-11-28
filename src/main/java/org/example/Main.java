613b3ae038bfc2ed804607167085f1eb9c7b54ea
santiago <124795721+santttimunoz@users.noreply.github.com>
2023-11-28 11:31:59 -0500

    se inicia

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    //Natillera la abuelita toretto
+    //¿Que necesito para procesar la natillera de 1 cliente?
+    //ENTRADAS
+    int identificadorCliente=2222;
+    long documentoIdentidad=100736187022L;
+    byte edadCliente=34;
+    short CuotaMensual=25000;
+    float tasaInteresFijo=0.1F;
+    char calificacionCliente=}'A';
+
+
+    }
+
+}
